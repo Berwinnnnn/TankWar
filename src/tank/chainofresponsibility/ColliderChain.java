73965e3ea2c6d500c4d8a4715768abba27cc9511
Berwin <979201197@qq.com>
2020-01-19 12:23:11 +0800

    通过上下左右箭头的按键状态，判定坦克的移动方向

--- src/tank/chainofresponsibility/ColliderChain.java
+++ src/tank/chainofresponsibility/ColliderChain.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class ColliderChain implements Collider{
     private List<Collider> colliders;
 
-    public ColliderChain() {
+    public ColliderChain() { 
         initColliders();
     }
 
